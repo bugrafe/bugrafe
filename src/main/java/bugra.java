4afe4a750306735b1e20d5fe3dd2b15aaa494720
Buğra feriz <yusufbugraferiz@hotmail.com>
2023-10-31 21:38:03 +0300

    dd

---
+++ src/main/java/bugra.java
@@ -0,0 +1,20 @@
+public class bugra {
+    public static void main(String[] args) {
+
+
+        //version 1 için kodlar
+        int x=1;
+        int y=2;
+
+
+        System.out.println(x+y);
+        System.out.println("Versiyon 1 tamamlandı");
+
+
+
+
+
+
+
+    }
+}
